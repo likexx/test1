57f79b6a0b9bf29dd91958c8ae6a26821d6ce9d6
likexx <likexx@live.com>
2016-02-27 03:28:54 -0800

    Update curl_test.sh

--- test/src/curl_test.sh
+++ test/src/curl_test.sh
@@ -7,3 +7,5 @@ curl -XPUT http://192.168.99.100/v1/journals/56d15d78ff76b2f469b219ca -H "Conten
 curl -XPOST http://192.168.99.100/v1/journals -H "Content-Type: application/json" -d '{"title":"BOOK 100"}'
 
 curl -XDELETE http://192.168.99.100/v1/journals/56d16cb0f457930f004efd2d -H "Content-Type: application/json"
+
+curl -XGET http://192.168.99.100/v1/journals/suggest/mon -H "Content-Type: application/json"
